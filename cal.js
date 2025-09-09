7e4bf8d8635d7946cb2af133b0382bd06144d9eb
Your Name <rajatpandey542@gmail.com>
2025-09-10 00:20:44 +0530

    refresh error solved

--- cal.js
+++ cal.js
@@ -1,17 +1,16 @@
 // let a, b;
 
 // function num1() {
-//     a = prompt("num1 = ")
+//     a = 
 //     return a;
 // }
 // function num2() {
-//     b = prompt("num2 = ")
+//     b = 
 //     return b;
 // }
 
 // function operator() {
-//     let choose = prompt("Choose the operator");
-//     return choose;
+    
 // }
 
 // function addition() {
@@ -61,7 +60,7 @@
 //             console.log(error);
 
 //     }
-//}
+// }
 // const buttons = document.querySelectorAll("button");
 
 // buttons.forEach(btn => {
@@ -90,92 +89,120 @@ value2.addEventListener("click", (e) => {
 const value3 = document.querySelector(".click3");
 value3.addEventListener("click", (e) => {
     console.log("three");
-    // output.style.color = "white";
-    // output.style.fontSize = "40px";
+    output.style.color = "white";
+    output.style.fontSize = "40px";
     output.value += "3";
 })
 
 const del = document.querySelector(".clickC");
 del.addEventListener("click", (e) => {
     console.log("Delete");
+    output.style.color = "white";
+    output.style.fontSize = "40px";
     output.value = "";
 })
 
 const zero = document.querySelector(".click0");
 zero.addEventListener("click", (e) => {
     console.log("zero");
+    output.style.color = "white";
+    output.style.fontSize = "40px";
     output.value += "0";
 })
 
 const value4 = document.querySelector(".click4");
 value4.addEventListener("click", (e) => {
     console.log("zero");
+    output.style.color = "white";
+    output.style.fontSize = "40px";
     output.value += "4";
 })
 
 const value5 = document.querySelector(".click5");
 value5.addEventListener("click", (e) => {
     console.log("zero");
+    output.style.color = "white";
+    output.style.fontSize = "40px";
     output.value += "5";
 })
 
 const value6 = document.querySelector(".click6");
 value6.addEventListener("click", (e) => {
     console.log("zero");
+    output.style.color = "white";
+    output.style.fontSize = "40px";
     output.value += "6";
 })
 
 const value7 = document.querySelector(".click7");
 value7.addEventListener("click", (e) => {
     console.log("zero");
+    output.style.color = "white";
+    output.style.fontSize = "40px";
     output.value += "7";
 })
 
 const value8 = document.querySelector(".click8");
 value8.addEventListener("click", (e) => {
     console.log("zero");
+    output.style.color = "white";
+    output.style.fontSize = "40px";
     output.value += "8";
 })
 
 const value9 = document.querySelector(".click9");
 value9.addEventListener("click", (e) => {
     console.log("zero");
+    output.style.color = "white";
+    output.style.fontSize = "40px";
     output.value += "9";
 })
 
 const valuedot = document.querySelector(".dot");
 valuedot.addEventListener("click", (e) => {
     console.log("zero");
+    output.style.color = "white";
+    output.style.fontSize = "40px";
     output.value += ".";
 })
 
 const operationPlus = document.querySelector(".add");
 operationPlus.addEventListener("click", (e) => {
     console.log("zero");
+    output.style.color = "white";
+    output.style.fontSize = "40px";
     output.value += " + ";
 })
 
 const operationMultiply = document.querySelector(".multiply");
 operationMultiply.addEventListener("click", (e) => {
     console.log("zero");
+    output.style.color = "white";
+    output.style.fontSize = "40px";
     output.value += " x ";
 })
 
 const operationDivide = document.querySelector(".divide");
 operationDivide.addEventListener("click", (e) => {
     console.log("zero");
+    output.style.color = "white";
+    output.style.fontSize = "40px";
     output.value += " ÷ ";
 })
 
 const operationPercent = document.querySelector(".percent");
 operationPercent.addEventListener("click", (e) => {
     console.log("zero");
+    output.style.color = "white";
+    output.style.fontSize = "40px";
     output.value += " % ";
 })
 
 const operationMinus = document.querySelector(".minus");
 operationMinus.addEventListener("click", (e) => {
     console.log("zero");
+    output.style.color = "white";
+    output.style.fontSize = "40px";
     output.value += " − ";
 })
 
